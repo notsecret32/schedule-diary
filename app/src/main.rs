73fc10e3a1e4de2404bc7bef7040ea8df7752064
notsecret32 <sashaskvorhevskiy@gmail.com>
2024-05-29 17:04:39 +0300

    feat: added gui project

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,7 +1,7 @@
+use tauri::Builder;
+use teloxide::prelude::*;
+use teloxide::types::Update;
 use dotenv::dotenv;
-use teloxide::{prelude::*, utils::command::BotCommands};
-use teloxide::types::{InlineKeyboardButton, InlineKeyboardMarkup};
-use url::Url;
 
 #[tokio::main]
 async fn main() {
@@ -15,27 +15,16 @@ async fn main() {
 
     let bot = Bot::new(token);
 
-    Command::repl(bot, answer).await;
-}
+    let tauri_app = Builder::default()
+        .on_message(|msg| {
+            if let Some(update) = Update::extract_message(&msg.payload) {
+                bot.handle_update(update);
+            }
 
-#[derive(BotCommands, Clone)]
-#[command(rename_rule = "lowercase", description = "These commands are supported:")]
-enum Command {
-    #[command(description = "Open Web App")]
-    OpenWebApp,
-}
+            Ok(())
+        })
+        .build(tauri::generate_context!())
+        .expect("Error while building Tauri application");
 
-async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
-    let web_app_url = dotenv::var("WEB_APP_URL").expect("Failed to get a WEB_APP_URL");
-
-    let button = InlineKeyboardButton::url("Открыть веб-приложение", Url::parse(&web_app_url).expect("Invalid URL"));
-
-    // Создайте разметку для кнопки
-    let keyboard = InlineKeyboardMarkup::default().append_row(vec![button]);
-
-    match cmd {
-        Command::OpenWebApp => bot.send_message(msg.chat.id, Command::descriptions().to_string()).reply_markup(keyboard).await?,
-    };
-
-    Ok(())
+    tauri_app.run();
 }
